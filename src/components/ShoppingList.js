1ad5ba1a06e440a51f4b13bd87d9eb900a2c1de8
Hana Sambora <grace.sambora@gmail.com>
2023-08-21 23:30:15 -0500

    Filter component is working

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -2,24 +2,53 @@ import React, { useState } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
+import { v4 as uuid } from "uuid";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [searchItem, setSearchItem] = useState("");
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+  function handleItemSearch(event) {
+    //console.log("handling")
+    setSearchItem(event.target.value);
+  }
 
+  const itemsToDisplay = items.filter((item) => {
+    if (selectedCategory === "All"){
+      if (searchItem === ""){
+        return true;
+      }
+      return item.name.toLowerCase().includes(searchItem.toLowerCase());
+    } 
     return item.category === selectedCategory;
   });
 
+ const [itemName, setNewName] = useState("");
+ const [itemCategory, setNewCategory] = useState("All");
+
+ function handleNewItem(event) {
+  event.preventDefault();
+
+  setNewName(event.target[0].value);
+  setNewCategory(event.target[1].value);
+
+  const newItem = {
+    id: uuid(), // the `uuid` library can be used to generate a unique id
+    name: itemName,
+    category: itemCategory,
+  };
+  
+  console.log(newItem);
+ }
+
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm onItemFormSubmit = {handleNewItem} />
+      <Filter search = {searchItem} onSearchChange = {handleItemSearch} onCategoryChange={handleCategoryChange} />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
